5c5bdc76407164a9a33106521986e70ba4cec52a
Georgina Van Dort and Shradha Pulla <georgina+shradha@>
2016-08-24 13:54:33 -0700

    Create shopping cart total price on index.js, create totalPrice function in shopping-cart.js

--- app/models/meal.js
+++ app/models/meal.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend ({
   name: DS.attr(),
   description: DS.attr(),
-  price: DS.attr(),
+  price: DS.attr("number"), //"number" assigns type to input
   ingredients: DS.attr(),
   spicyScale: DS.attr(),
   imgURL: DS.attr(),
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
-export default Ember.Service.extend({
+export default Ember.Service.extend ({
   ordersList:[],
-
   add(mealItem) {
     this.get('ordersList').pushObject(mealItem);
-  }
+  },
+  totalPrice: Ember.computed("ordersList.[]", function() {
+    var totalPrice = 0;
+    var ordersList = this.get("ordersList"); //Have to create var again to local
+    for (var i = 0; i < ordersList.length; i ++) {
+      totalPrice += ordersList[i].get("price");
+    }
+    return totalPrice.toFixed(2);
+  })
 });
