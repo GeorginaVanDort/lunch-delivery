d29f111d4cd8b4e4425cf006ce70443613110b99
Georgina Van Dort and Shradha Pulla <georgina+shradha@>
2016-08-24 13:12:46 -0700

    Create update meal functionality on admin

---
+++ app/components/update-meal.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend ({
+  updateMealForm: false,
+  actions: {
+    showUpdateMealForm() {
+      this.set("updateMealForm", true);
+    },
+    updateMeal(meal) {
+      var params = {
+        name: this.get("name"),
+        description: this.get("description"),
+        price: this.get("price"),
+        ingredients: this.get("ingredients"),
+        spicyScale: this.get("spicyScale"),
+        imgURL: this.get("imgURL"),
+        popularity: 0
+      };
+      this.set("updateMealForm", false);
+      this.sendAction("updateMeal", meal, params);
+      this.set("name", "");
+      this.set("description", "");
+      this.set("price", "");
+      this.set("ingredients", "");
+      this.set("spicyScale", "");
+      this.set("imgURL", "");
+    }
+  }
+});
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend ({
+  model() {
+    return this.store.findAll("meal");
+  },
   actions: {
     saveMeal(params) {
       var newMeal = this.store.createRecord("meal", params);
       newMeal.save();
+      this.transitionTo("admin");
+    },
+    updateMeal(meal, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          meal.set(key, params[key]);
+        }
+      });
+      meal.save();
       this.transitionTo("admin");
     }
   }
